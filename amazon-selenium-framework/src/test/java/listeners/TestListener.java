e196151b92a34bb20787490d1e7ca5e21cc3c1d6
suchismitasingh9944@gmail.com <Suchismitasingh9944@gmail.com>
2026-09-19 09:05:53 +0530

    practice test

--- amazon-selenium-framework/src/test/java/listeners/TestListener.java
+++ amazon-selenium-framework/src/test/java/listeners/TestListener.java
@@ -26,6 +26,7 @@ public class TestListener implements ITestListener {
                 context.getPassedTests().size(),
                 context.getFailedTests().size(),
                 context.getSkippedTests().size());
+                context.getSkippedTests().size());
     }
 
     @Override
